ced4cdee9359ba5415d82e4bc9430cda0ba3b55b
ananas32 <porsche839@gmail.com>
2017-03-10 23:49:29 +0200

    add language validation: ru, uk

---
+++ resources/lang/ru/menu.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'news' => 'Новости',
+    'search' => 'Поиск тарифу',
+    'tariff' => 'Таривные планы',
+    'guestbook' => 'Оставить отзыв',
+    'choose_region' => 'Выбрать область'
+];
