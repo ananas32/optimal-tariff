d987f358e87ecc0ad82b924c864903901b0896dd
Vitaliy.Loshniy <vitaliy.loshniy@edipresse.ua>
2018-03-14 10:48:03 +0200

    add message table

---
+++ database/migrations/2018_03_14_103740_create_message_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->boolean('unlimited');
+            $table->string('tariff_message');
+            $table->string('quantity');
+            $table->string('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
