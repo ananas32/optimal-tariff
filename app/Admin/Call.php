ef375e568ab0e34a284f42d72d3a6ab39a0eba80
anans32 <porsche839@gmail.com>
2018-03-18 18:45:43 +0200

    fix bug and new models

--- app/Admin/Call.php
+++ app/Admin/Call.php
@@ -7,30 +7,31 @@ AdminSection::registerModel(Call::class, function (ModelConfiguration $model) {
 
 //    $model->enableAccessCheck();
 
-	$model->setTitle('Звонки');
+    $model->setTitle('Звонки');
 
-	$model->onDisplay(function () {
-		$display = AdminDisplay::datatables();
+    $model->onDisplay(function () {
+        $display = AdminDisplay::datatables();
 
-		$display->setColumns([
-			AdminColumn::custom()->setLabel('Visible')->setCallback(function ($instance) {
-				return $instance->unread ? '<i class="fa fa-check bg-success"></i>' : '<i class="fa fa-minus text-danger bg-danger"></i>';
-			})->setWidth('50px')->setHtmlAttribute('class', 'text-center'),
-			AdminColumn::email('email')->setLabel('User email'),
-			AdminColumn::text('username')->setLabel('User name')
-		]);
-		return $display;
-	});
-	// Create And Edit
-	$model->onCreateAndEdit(function() {
-		return $form = AdminForm::panel()->addBody(
-			AdminFormElement::checkbox('unread', 'Visible'),
-			AdminFormElement::text('username', 'User name')->setReadonly(true),
-			AdminFormElement::text('email', 'email')->setReadonly(true),
-			AdminFormElement::textarea('message', 'Message')->setReadonly(true),
-			AdminFormElement::textarea('answer', 'Answer'),
-			AdminFormElement::text('ip_address', 'Ip address')->required()
-		);
-	});
+        $display->setColumns([
+            AdminColumn::text('name')->setLabel('Название'),
+            AdminColumn::custom()->setLabel('Безлимит')->setCallback(function ($instance) {
+                return $instance->unlimited ? '<i class="fa fa-check bg-success"></i>' : '<i class="fa fa-minus text-danger bg-danger"></i>';
+            })->setWidth('50px')->setHtmlAttribute('class', 'text-center'),
+            AdminColumn::text('tariff_minute')->setLabel('Минут в тарифе'),
+            AdminColumn::text('quantity')->setLabel('Количество'),
+            AdminColumn::text('price')->setLabel('Цена'),
+        ]);
+        return $display;
+    });
+    // Create And Edit
+    $model->onCreateAndEdit(function() {
+        return $form = AdminForm::panel()->addBody(
+            AdminFormElement::text('name', 'Название')->setValidationRules(['name' => 'required|string|min:2|max:190']),
+            AdminFormElement::checkbox('unlimited', 'Безлимит'),
+            AdminFormElement::text('tariff_minute', 'Минут в тарифе')->setValidationRules(['tariff_minute' => 'numeric']),
+            AdminFormElement::text('quantity', 'Количество минут')->setValidationRules(['quantity' => 'numeric']),
+            AdminFormElement::text('price', 'Цена')->setValidationRules(['price' => 'numeric'])
+        );
+    });
 
 });
--- app/Admin/InternetPackage.php
+++ app/Admin/InternetPackage.php
@@ -13,32 +13,25 @@ AdminSection::registerModel(InternetPackage::class, function (ModelConfiguration
 		$display = AdminDisplay::datatables();
 
 		$display->setColumns([
-			AdminColumn::text('name')->setLabel('Название для звонка'),
+			AdminColumn::text('name')->setLabel('Название'),
 			AdminColumn::custom()->setLabel('Безлимит')->setCallback(function ($instance) {
 				return $instance->unlimited ? '<i class="fa fa-check bg-success"></i>' : '<i class="fa fa-minus text-danger bg-danger"></i>';
 			})->setWidth('50px')->setHtmlAttribute('class', 'text-center'),
-			AdminColumn::text('tariff_minute')->setLabel('Минут в тарифе'),
+			AdminColumn::text('tariff_package')->setLabel('Мегабайт в тарифе'),
 			AdminColumn::text('quantity')->setLabel('Количество'),
-			AdminColumn::text('price')->setLabel('Название для звонка'),
+			AdminColumn::text('price')->setLabel('Цена'),
 		]);
 		return $display;
 	});
 	// Create And Edit
 	$model->onCreateAndEdit(function() {
-		return $form = AdminForm::panel()->addBody(
-			AdminFormElement::checkbox('unread', 'Visible'),
-			AdminFormElement::text('username', 'User name')->setReadonly(true),
-			AdminFormElement::text('email', 'email')->setReadonly(true),
-			AdminFormElement::textarea('message', 'Message')->setReadonly(true),
-			AdminFormElement::textarea('answer', 'Answer'),
-			AdminFormElement::text('ip_address', 'Ip address')->required()
+		return AdminForm::panel()->addBody(
+            AdminFormElement::text('name', 'Название')->setValidationRules(['name' => 'required|string|min:2|max:190']),
+            AdminFormElement::checkbox('unlimited', 'Безлимит'),
+            AdminFormElement::text('tariff_package', 'Мегабайт в тарифе')->setValidationRules(['tariff_package' => 'numeric']),
+            AdminFormElement::text('quantity', 'Количество минут')->setValidationRules(['quantity' => 'numeric']),
+            AdminFormElement::text('price', 'Цена')->setValidationRules(['price' => 'numeric'])
 		);
 	});
 
 });
-
-$table->string('name');
-$table->boolean('unlimited');
-$table->string('tariff_minute');
-$table->string('quantity');
-$table->string('price');
